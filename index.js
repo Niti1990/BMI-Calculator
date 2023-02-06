fddb0281bae061751b8fc032ee837ba71ea9e9db
nitika <nitikadiwakar.bajaj@gmail.com>
2023-02-06 18:02:03 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function BMI() {
+	let height = document.getElementById('height').value
+	let weight = document.getElementById('weight').value
+	let bmi = weight / (((height / 100) * height) / 100)
+	let bmio = bmi.toFixed(2)
+	document.getElementById('result').innerHTML = 'Your BMI is:' + bmio;
+}
